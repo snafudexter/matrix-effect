e1371a91b06caba7cd4aa511d7df52249fa719e8
snafudexter <prabh@live.in>
2020-04-19 11:06:10 +0530

    full origin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let headers = new Headers({"Content-Type": 'text/html'});
+headers.append('Access-Control-Allow-Origin','https://codifyinditest.com')
 
-fetch('https://codifyinditest.com/script_test/api-test',{
+fetch('https://codifyinditest.com/script_test/api-test/',{
     headers,
     credentials: 'same-origin'
 }).then(function(response){
